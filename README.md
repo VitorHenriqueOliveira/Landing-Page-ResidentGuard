d848ff952c244f46efead5bb265068327908654c
Vitor Oliveira <vitonciogames@gmail.com>
2025-10-19 17:20:58 -0300

    Atualização geral e inserção do link oficial.

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
 <p align="center">
-<picture>
-    <img src="img/logo.svg" alt="residentguard Logo" width="15%"> 
-</picture>
+    <picture>
+        <img src="img/logo.svg" alt="residentguard Logo" width="15%"> 
+    </picture>
+</p>
 
 <h1 align="center">Landing Page ResidentGuard</h1>
 
-### Landing Page do projeto ResidentGuard
+## 📝 O que é ResidentGuard?
 
 ResidentGuard é uma aplicação que tem como objetivo facilcar o controle de entrada e saída de um condomínio. Com o sistema, será possível organizar a entrega de encomendas, agendar o uso de áreas comuns, salvar as informações de moradores e visitantes no banco de dados, tudo de forma difital, rápida e organizada.
 
 Além de tudo, é um Trabalho de Conclusão de Curso apresentado como exigência Parcial para obtenção da Habilitação Profissional Técnica de Nível Médio de Técnico em Desenvolvimento de Sistemas.
+
+---
+
+## 🖥️ Como acessar?
+
+Para ter acesso a todas as funcionalidades da aplicação é necessário ter permissão administrativa através de nome de usuário e senha. Sendo apenas obtível pelo contato direto com os desenvolvedores, ainda é possível experimentar uma pequena amostra do projeto.
+
+**Acessar** <a href="http://residentguard.page.gd/">ResidentGuard.</a>
--- script.js
+++ script.js
@@ -1,67 +1,16 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', function () {
+    if (window.feather) {
+        feather.replace();
+    }
+});
 
-  const faqButtons = document.querySelectorAll(".faq-question");
-  faqButtons.forEach(button => {
-    button.addEventListener("click", () => {
-      const answer = button.nextElementSibling;
-      document.querySelectorAll(".faq-answer").forEach(a => {
-        if(a !== answer) a.style.maxHeight = null;
-      });
-      if(answer.style.maxHeight){
-        answer.style.maxHeight = null;
-      } else {
-        answer.style.maxHeight = answer.scrollHeight + "px";
-      }
-    });
-  });
-     function achar_proximo (elemento, lista, index) {
-      const tamanho = lista.length - 1;
-
-      let index_futuro = index + 1;
-
-      if (tamanho == index) {
-        index_futuro = 0;
-      }
-
-      let index_passado = index - 1;
-
-      if (index_passado == - 1){
-        index_passado = tamanho;
-      }
-
-      lista[index_passado].classList.add('past');
-      lista[index_futuro].classList.add('future');
-     
-     }
-
-     function alterar_imagem(elemento, lista, index) {
-      let past = document .querySelector(".past");
-      let future = document .querySelector(".future");
-      
-      elemento.classList.add('hidden');
-      future.classList.remove("hidden", "future");
-      past.classList.remove("past");
-      index += 1;
-      achar_proximo(future, lista, index);
-     }
-
-     let index = 0;
-    document.addEventListener('click', function(event) 
-    {
-
-        let carousel = event.target.closest('.elemento-carousel');
-        console.log(index);
-        if (carousel) {
-          const list_carousel = document.querySelectorAll(".elemento-carousel");
-          if(list_carousel){
-              if (index == 0) {
-                achar_proximo(carousel, list_carousel, index);
-              }
-              alterar_imagem(carousel, list_carousel, index)
-              index += 1;
-          }
-      }
-      
-  
+// Garantir rolagem suave entre os sections além do css.
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute("href"));
+        if (target) {
+            target.scrollIntoView({ behavior: "smooth", block: "start" });
+        }
     });
 });
